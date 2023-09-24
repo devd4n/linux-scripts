52c0d944a0df332bbe82cc21b812c4790c504f75
devd4n <18461526+devd4n@users.noreply.github.com>
2023-09-24 12:23:37 +0200

    Update deb-apt-key-2.sh

--- deb-apt-key-2.sh
+++ deb-apt-key-2.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 # download this script to the bin folder
-# wget -O /usr/bin/apt-add-repo https://raw.githubusercontent.com/devd4n/linux-scripts/main/deb-apt-key-2.sh
+# wget -qO /usr/bin/apt-add-repo https://raw.githubusercontent.com/devd4n/linux-scripts/main/deb-apt-key-2.sh
 # Run as sudo
 repo_name=$0
 repo_uri=$1
 key_uri=$2
 
 wget -q -O key.gpg $key_uri
-typecount=$(file key.gpg | grep -c)
+typecount=$(file key.gpg | grep -c "PGP public key block Public-Key")
 if [ $typecount >= 0 ]
-gpg --no-default-keyring --keyring ./tmp.gpg --import key.gpg
-gpg --no-default-keyring --keyring ./tmp.gpg --export --output $repo_name.gpg
-rm tmp.gpg key.gpg
-sudo mv $repo_name.gpg /etc/apt/keyrings/$repo_name.gpg
+then
+  gpg --no-default-keyring --keyring ./tmp.gpg --import key.gpg
+  gpg --no-default-keyring --keyring ./tmp.gpg --export --output $repo_name.gpg
+  rm tmp.gpg
+  sudo mv $repo_name.gpg /etc/apt/keyrings/$repo_name.gpg
+else
+  sudo mv $key.gpg /etc/apt/keyrings/$repo_name.gpg
+fi
+rm $key.gpg
+
 #  Write sources list file in DEB822 format
 # 
 echo "Types: deb" > /etc/apt/sources.list.d/$repo_name.sources
