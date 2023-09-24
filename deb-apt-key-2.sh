2ab493c0702703e10c7cfb80f24b6a199adeb47a
devd4n <18461526+devd4n@users.noreply.github.com>
2023-09-24 14:28:00 +0200

    Update deb-apt-key-2.sh

--- deb-apt-key-2.sh
+++ deb-apt-key-2.sh
@@ -4,9 +4,16 @@
 # make it executable
 # sudo chmod +x /usr/bin/apt-add-repo
 # Run as sudo
+paramsarray="${@}"
 repo_name=$1
-repo_uri=$2
-key_uri=$3
+key_uri=$2
+repo_uri=$3
+suite=$4
+unset paramsarray[1]
+unset paramsarray[2]
+unset paramsarray[3]
+unset paramsarray[4]
+components=$paramsarray
 
 wget -q -O key.gpg $key_uri
 typecount=$(file key.gpg | grep -c "PGP public key block Public-Key")
@@ -25,8 +32,8 @@ rm key.gpg
 # 
 echo "Types: deb deb-src" | sudo tee /etc/apt/sources.list.d/$repo_name.sources
 echo "URIs: $repo_uri" | sudo tee -a /etc/apt/sources.list.d/$repo_name.sources
-echo "Suites: any" | sudo tee -a /etc/apt/sources.list.d/$repo_name.sources
-echo "Components: main" | sudo tee -a /etc/apt/sources.list.d/$repo_name.sources
+echo "Suites: $suite" | sudo tee -a /etc/apt/sources.list.d/$repo_name.sources
+echo "Components: $components" | sudo tee -a /etc/apt/sources.list.d/$repo_name.sources
 echo "Signed-By: /etc/apt/keyrings/$repo_name.gpg" | sudo tee -a /etc/apt/sources.list.d/$repo_name.sources
 sudo apt update
 
